a6e11b980cb4ed571b83a0ec82689e4d2732a62b
Manuel Obregozo <obre41@gmail.com>
2016-08-14 20:29:20 -0300

    added filters to movie list

--- app/js/controllers/controller.movie.js
+++ app/js/controllers/controller.movie.js
@@ -6,21 +6,67 @@ angular.module('MovieTracker.controllers')
 .controller('MovieListCtrl', function($scope, movieService) {
 
 	$scope.movieList = [];
-
-
-	$scope.getMovies = function(){
-		var call = movieService.getMovies();
-		call.then(
-			function(payload){
-				$scope.movieList = payload.data;
-			}, 
-			function(){});
+	$scope.loadingMovies = false;
+	$scope.avalaibleMovies = false;
+	$scope.filters = {
+		page: 0,
+		size: 10,
+		query: '',
+		orderBy: 'genre',
+		desc: false,
+		availableOptions: [{
+			name: 'genre'
+		}, {
+			name: 'year'
+		}, {
+			name: 'name'
+		}, {
+			name: 'release date'
+		}],
+		order: {
+			name: 'genre'
+		}
 	}
 
 
-    $scope.$on('$viewContentLoaded', function() {
-    	$scope.getMovies();
-    });
+	$scope.getMovies = function() {
+		$scope.loadingMovies = true;
+		var call = movieService.getMovies(
+			$scope.filters.page,
+			$scope.filters.size,
+			$scope.filters.query,
+			$scope.filters.order.name,
+			$scope.filters.desc);
+		call.then(
+			function(payload) {
+				$scope.movieList = $scope.movieList.concat(payload.data.movies);
+				$scope.avalaibleMovies = ($scope.movieList.length < $scope.filters.size) ? false : true;
+			},
+			function() {}).finally(function(){
+				$scope.loadingMovies = false;
+			})
+	}
+
+
+	$scope.$on('$viewContentLoaded', function() {
+		$scope.getMovies();
+	});
+
+	$scope.loadMoreMovies = function() {
+        if (!$scope.loadingMovies && $scope.avalaibleMovies) {
+            $scope.filters.page= $scope.filters.page+1;
+            $scope.getMovies();
+        }
+    }
+
+	$scope.$watch('filters', function(newVal, oldVal) {
+		if (!$scope.loadingMovies && newVal!= oldVal) {
+			$scope.filters.page= 0;
+		$scope.movieList = [];
+		$scope.getMovies();
+        }
+		
+	}, true);
 
 
 })
@@ -30,22 +76,22 @@ angular.module('MovieTracker.controllers')
 //  Home Controller 
 .controller('MovieProfileCtrl', function($scope, movieService, $stateParams) {
 
-	$scope.movieData= {};
+	$scope.movieData = {};
 
-	$scope.getMovieProfile = function(){
+	$scope.getMovieProfile = function() {
 		var call = movieService.getMovieProfile($stateParams.movieId);
 		call.then(
-			function(payload){
+			function(payload) {
 				$scope.movieData = payload.data;
-			}, 
-			function(){});
+			},
+			function() {});
 	}
 
-	
 
-    $scope.$on('$viewContentLoaded', function() {
-    	$scope.getMovieProfile();
-    });
+
+	$scope.$on('$viewContentLoaded', function() {
+		$scope.getMovieProfile();
+	});
 
 
 })
@@ -57,21 +103,19 @@ angular.module('MovieTracker.controllers')
 	$scope.movieList = [];
 
 
-	$scope.getMovies = function(){
+	$scope.getMovies = function() {
 		var call = movieService.getMovies();
 		call.then(
-			function(payload){
+			function(payload) {
 				$scope.movieList = payload.data;
-			}, 
-			function(){});
+			},
+			function() {});
 	}
 
 
-    $scope.$on('$viewContentLoaded', function() {
-    	$scope.getMovies();
-    });
+	$scope.$on('$viewContentLoaded', function() {
+		$scope.getMovies();
+	});
 
 
 })
-
-
--- app/js/services.js
+++ app/js/services.js
@@ -3,12 +3,35 @@
 angular.module('MovieTracker.services')
 
 .factory('movieService', function($http, config) {
+	return {
+		getMovies: function(page, size, query, order, desc) {
+			return $http.get(config.apiUrl + '/movies', {
+				params: {
+					page: page,
+					size: size,
+					query: query,
+					orderBy: order,
+					desc: desc
+				}
+			});
+		},
+		getMovieProfile: function(movieId) {
+			return $http.get(config.apiUrl + '/movie/' + movieId);
+		}
+	};
+})
+
+//Directive to handle when the scroll is at the bottom of an element
+.directive('scrolly', function() {
     return {
-        getMovies: function() {
-            return $http.get(config.apiUrl+'/movies');
-        },
-        getMovieProfile: function(movieId) {
-            return $http.get(config.apiUrl+'/movie/'+movieId);
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var raw = element[0];
+            element.bind('scroll', function() {
+                if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
+                    scope.$apply(attrs.scrolly);
+                }
+            });
         }
     };
 })
